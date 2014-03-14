7380c40a22c4f21713b8675efb9f96578dbcac3e
Subra <subra@orangebrush.net>
2014-03-14 18:53:39 +0800

    changing the module to use provide() instead

--- src/ng-aria.js
+++ src/ng-aria.js
@@ -1,4 +1,4 @@
-angular.module('ngAria', []).factory('$aria', [function(){
+angular.module('ngAria', []).provider('$aria', function(){
   var defaultConfig = {
     ariaHidden : true,
     ariaChecked: true,
@@ -11,8 +11,94 @@ angular.module('ngAria', []).factory('$aria', [function(){
     ariaChecked: false,
     ariaDisabled: false,
     ariaRequired: false,
-    ariaInvalid: true
-  };
+    ariaInvalid: false
+  }
+
+  var ariaFactory = function(){
+    return {
+      ariaHidden: function(scope, elem, attr){
+        if(currentConfig.ariaHidden){
+          var destroyWatcher = scope.$watch(function(){
+            return elem.attr('class');
+          }, function(){
+            if(elem.hasClass('ng-hide')){
+              elem.attr('aria-hidden', 'true');
+            }else{
+              elem.attr('aria-hidden', 'false');
+            }  
+          });
+          scope.$on('$destroy', function(){
+            destroyWatcher();
+          });
+        }
+      },
+      ariaChecked: function(scope, elem, attr){
+        if(currentConfig.ariaChecked){
+          var destroyWatcher = scope.$watch(function(){
+            return elem.attr('checked');
+          }, function(){
+            if(angular.isUndefined(elem.attr('checked'))){
+              elem.attr('aria-checked', 'false');
+            }else{
+              elem.attr('aria-checked', 'true');
+            }
+          });
+          scope.$on('$destroy', function(){
+            destroyWatcher();
+          });
+        }
+      },
+      ariaDisabled: function(scope, elem, attr){
+        if(currentConfig.ariaDisabled){
+          var destroyWatcher = scope.$watch(function(){
+            return elem.attr('disabled');
+          }, function(){
+            if(angular.isUndefined(elem.attr('disabled'))){
+              elem.attr('aria-disabled', 'false');
+            }else{
+              elem.attr('aria-disabled', 'true');
+            }
+          });
+          scope.$on('$destroy', function(){
+            destroyWatcher();
+          });
+        }
+      },
+      ariaRequired: function(scope, elem, attr){
+        if(currentConfig.ariaRequired){
+          var destroyWatcher = scope.$watch(function(){
+            return elem.attr('required');
+          }, function(){
+            if(angular.isUndefined(elem.attr('required'))){
+              elem.attr('aria-required', 'false');
+            }else{
+              elem.attr('aria-required', 'true');
+            }
+          });
+          scope.$on('$destroy', function(){
+            destroyWatcher();
+          });
+        }
+      },
+      ariaInvalid: function(scope, elem, attr){
+        if(currentConfig.ariaInvalid){
+          var destroyWatcher = scope.$watch(function(){
+            return elem.attr('class');
+          }, function(){
+            if(elem.hasClass('ng-invalid')){
+              elem.attr('aria-invalid', 'true');
+            }else{
+              elem.attr('aria-invalid', 'false');
+            }
+          });
+          scope.$on('$destroy', function(){
+            destroyWatcher();
+          });
+        }
+      }
+    };
+  }
+
   return{
     enable: function(config){
       if(angular.isUndefined(config)){
@@ -24,133 +110,50 @@ angular.module('ngAria', []).factory('$aria', [function(){
     setConfig: function(config){
       currentConfig = angular.extend(defaultConfig, config);
     },
-    getConfig : function(){
-      return currentConfig;
-    }
-  };
-}]).factory('$ariaFns', ['$aria', function($aria){
-  return {
-    ariaHidden: function(scope, elem, attr){
-      if($aria.getConfig().ariaHidden){
-        var destroyWatcher = scope.$watch(function(){
-          return elem.attr('class');
-        }, function(){
-          if(elem.hasClass('ng-hide')){
-            elem.attr('aria-hidden', 'true');
-          }else{
-            elem.attr('aria-hidden', 'false');
-          }  
-        });
-        scope.$on('$destroy', function(){
-          destroyWatcher();
-        });
-      }
-    },
-    ariaChecked: function(scope, elem, attr){
-      if($aria.getConfig().ariaChecked){
-        var destroyWatcher = scope.$watch(function(){
-          return elem.attr('checked');
-        }, function(){
-          if(angular.isUndefined(elem.attr('checked'))){
-            elem.attr('aria-checked', 'false');
-          }else{
-            elem.attr('aria-checked', 'true');
-          }
-        });
-        scope.$on('$destroy', function(){
-          destroyWatcher();
-        });
-      }
-    },
-    ariaDisabled: function(scope, elem, attr){
-      if($aria.getConfig().ariaDisabled){
-        var destroyWatcher = scope.$watch(function(){
-          return elem.attr('disabled');
-        }, function(){
-          if(angular.isUndefined(elem.attr('disabled'))){
-            elem.attr('aria-disabled', 'false');
-          }else{
-            elem.attr('aria-disabled', 'true');
-          }
-        });
-        scope.$on('$destroy', function(){
-          destroyWatcher();
-        });
-      }
-    },
-    ariaRequired: function(scope, elem, attr){
-      if($aria.getConfig().ariaRequired){
-        var destroyWatcher = scope.$watch(function(){
-          return elem.attr('required');
-        }, function(){
-          if(angular.isUndefined(elem.attr('required'))){
-            elem.attr('aria-required', 'false');
-          }else{
-            elem.attr('aria-required', 'true');
-          }
-        });
-        scope.$on('$destroy', function(){
-          destroyWatcher();
-        });
-      }
-    },
-    ariaInvalid: function(scope, elem, attr){
-      if($aria.getConfig().ariaInvalid){
-        var destroyWatcher = scope.$watch(function(){
-          return elem.attr('class');
-        }, function(){
-          if(elem.hasClass('ng-invalid')){
-            elem.attr('aria-invalid', 'true');
-          }else{
-            elem.attr('aria-invalid', 'false');
-          }
-        });
-        scope.$on('$destroy', function(){
-          destroyWatcher();
-        });
-      }
-    }
-  };
-}]).directive('ngShow', ['$ariaFns', function($ariaFns){
-  return $ariaFns.ariaHidden;
-}]).directive('ngHide', ['$ariaFns', function($ariaFns){
-  return $ariaFns.ariaHidden;
-}]).directive('input', ['$ariaFns', function($ariaFns){
+    $get: ariaFactory
+  }
+
+  
+}).directive('ngShow', ['$aria', function($aria){
+  return $aria.ariaHidden;
+}]).directive('ngHide', ['$aria', function($aria){
+  return $aria.ariaHidden;
+}]).directive('input', ['$aria', function($aria){
   return{
     restrict: 'E',
     link: function(scope, elem, attr){
       if(attr.type === 'checkbox'){
-        $ariaFns.ariaChecked(scope, elem, attr);
+        $aria.ariaChecked(scope, elem, attr);
       }
-      $ariaFns.ariaDisabled(scope, elem, attr);
-      $ariaFns.ariaRequired(scope, elem, attr);
-      $ariaFns.ariaInvalid(scope, elem, attr);
+      $aria.ariaDisabled(scope, elem, attr);
+      $aria.ariaRequired(scope, elem, attr);
+      $aria.ariaInvalid(scope, elem, attr);
     }
   };
-}]).directive('textarea', ['$ariaFns', function($ariaFns){
+}]).directive('textarea', ['$aria', function($aria){
   return{
     restrict: 'E',
     link: function(scope, elem, attr){
-      $ariaFns.ariaDisabled(scope, elem, attr);
-      $ariaFns.ariaRequired(scope, elem, attr);
-      $ariaFns.ariaInvalid(scope, elem, attr);
+      $aria.ariaDisabled(scope, elem, attr);
+      $aria.ariaRequired(scope, elem, attr);
+      $aria.ariaInvalid(scope, elem, attr);
     }
   };
-}]).directive('button', ['$ariaFns', function($ariaFns){
+}]).directive('button', ['$aria', function($aria){
   return{
     restrict: 'E',
     link: function(scope, elem, attr){
-      $ariaFns.ariaDisabled(scope, elem, attr);
-      $ariaFns.ariaRequired(scope, elem, attr);
+      $aria.ariaDisabled(scope, elem, attr);
+      $aria.ariaRequired(scope, elem, attr);
     }
   };
-}]).directive('select', ['$ariaFns', function($ariaFns){
+}]).directive('select', ['$aria', function($aria){
   return{
     restrict: 'E',
     link: function(scope, elem, attr){
-      $ariaFns.ariaDisabled(scope, elem, attr);
-      $ariaFns.ariaRequired(scope, elem, attr);
-      $ariaFns.ariaInvalid(scope, elem, attr);
+      $aria.ariaDisabled(scope, elem, attr);
+      $aria.ariaRequired(scope, elem, attr);
+      $aria.ariaInvalid(scope, elem, attr);
     }
   };
 }]);
